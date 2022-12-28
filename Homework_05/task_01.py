5b9320afaf93ac2a0d9ddef0b48e01eca832ea6e
DmytriiTsiupko <tsiupkodmitrii@gmail.com>
2022-12-28 12:52:21 +0100

    Homework 05: Fibonacci sequence by generator, by iterator, by recursion and finding the factorial of a number by recursion.
    - 1. Create a program that will receive a number and print the corresponding number in the Fibonacci list using generators
    
    - 2. Create a program that will accept a number and print the corresponding number in the Fibonacci sequence using iterators
    
    - 3. Create a program that will accept a number and print the corresponding number in the Fibonacci sequence using recursion
    
    - 4. Write a program that will return the factorial of the entered number using recursion
    
    Example: when entering 3 - output 2, when entering 6 - output 8, when entering 9 - output 34.

---
+++ Homework_05/task_01.py
@@ -0,0 +1,16 @@
+# 1. Create a program that will receive a number and print the corresponding number in the Fibonacci list using
+# generators
+print("### Fibonacci Generator ###")
+while True:
+    def get_fibonacci(number):   # create generator
+        fib1 = fib2 = 1
+        for i in range(number):
+            yield fib1
+            fib1, fib2 = fib2, fib1 + fib2
+
+    input_number = int(input("-> "))
+    generator_fibonacci = get_fibonacci(input_number)  # create list comprehension
+
+    fibonacci_list = [numb for numb in generator_fibonacci]
+
+    print(f"{input_number} number in the Fibonacci sequence: {fibonacci_list[-1]}\n")
---
+++ Homework_05/task_02.py
@@ -0,0 +1,32 @@
+# 2. Create a program that will accept a number and print the corresponding number in the Fibonacci sequence using
+# iterators
+
+class Fibonacci:
+
+    def __init__(self, max_number):
+        self.max_number = max_number
+        self.counter = 0
+        self.fib1 = 1
+        self.fib2 = 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.counter += 1
+        self.fib = self.fib1
+
+        if self.counter > self.max_number:
+            raise StopIteration
+        self.fib1, self.fib2 = self.fib2, self.fib1 + self.fib2
+        return self.fib
+
+
+print("### Fibonacci Iterator ###")
+
+while True:
+    fibonacci_sequence = Fibonacci(int(input("-> ")))
+
+    fibonacci_list = [numb for numb in fibonacci_sequence]
+
+    print(f"{fibonacci_sequence.max_number} number in the Fibonacci sequence: {fibonacci_list[-1]}")
