3cf44fe95cf76a7251971cf3384e1642b1155f52
DmytriiTsiupko <tsiupkodmitrii@gmail.com>
2023-03-27 15:18:24 +0200

    Rewrite the program.

--- 15. API/task2_API_requests/pogoda.py
+++ 15. API/task2_API_requests/pogoda.py
@@ -1,33 +1,63 @@
 import requests
-from pprint import pprint
 
-while True:
-    city = input("Введіть назву міста: ")
 
-    # Отримання координат міста за допомогою API open-meteo.com
-    response = requests.get(f'https://geocoding-api.open-meteo.com/v1/search?name={city}')
-    if response.status_code == 200:
-        data = response.json()
-        # pprint(data)
-        # print(data['results'][0]['latitude'])
+class Pogoda:
 
-        if 'error' in data:
-            print(f"Помилка: {data['error']['message']}")
+    geocoding_api_open_meteo = "https://geocoding-api.open-meteo.com/v1/search?name="
+
+    def __init__(self, city: str):
+        self.city = city
+        self.session = requests.Session()
+
+    # Отримання координат міста за допомогою API
+    def get_cord(self):
+        response = self.session.get(f"{self.geocoding_api_open_meteo}{self.city}")
+
+        try:
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as err:
+            print(f"HTTP error occurred: {err}")
+        except requests.exceptions.RequestException as err:
+            print(f"Other error occurred: {err}")
         else:
-            lat, lon = data['results'][0]['latitude'], data['results'][0]['longitude']
-            # Отримання поточних показників погоди за допомогою API open-meteo.com
-            response = requests.get(f'https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true')
-            if response.status_code == 200:
-                data = response.json()
-                if 'error' in data:
-                    print(f"Помилка: {data['error']['message']}")
-                else:
-                    current_weather = data['current_weather']
-                    temperature = current_weather['temperature']
-                    time = current_weather['time']
-                    windspeed = current_weather['windspeed']
-                    print(f"Погода в місті {city}.\nЧас: {time}\nТемпература: {temperature}°C\nШвидкість вітру: {windspeed} м/с\n")
+            data = response.json()
+            return data.get("results", "Помилка при отриманні координат міста")
+
+    def get_weather_by_cord(self):
+        cord_data = self.get_cord()
+        if not isinstance(cord_data, str):
+            lat, lon = cord_data[0]["latitude"], cord_data[0]["longitude"]
+            response = self.session.get(
+                f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true"
+            )
+
+            try:
+                response.raise_for_status()
+            except requests.exceptions.HTTPError as err:
+                print(f"HTTP error occurred: {err}")
+            except requests.exceptions.RequestException as err:
+                print(f"Other error occurred: {err}")
             else:
-                print("Помилка при отриманні даних погоди")
-    else:
-        print("Помилка при отриманні координат міста")
+                cord_data = response.json()
+
+            try:
+                current_weather = cord_data["current_weather"]
+                temperature = current_weather["temperature"]
+                time = current_weather["time"]
+                windspeed = current_weather["windspeed"]
+                print(
+                    f"Погода в місті {self.city}.\nЧас: {time}\n"
+                    f"Температура: {temperature}°C\n"
+                    f"Швидкість вітру: {windspeed} м/с\n"
+                )
+            except KeyError as key_error:
+                print(f"Key Error: {key_error}")
+        else:
+            print(cord_data)
+
+
+if __name__ == "__main__":
+    while True:
+        local_weather = Pogoda(input("Введіть назву міста: ").strip().capitalize())
+        local_weather.get_weather_by_cord()
+
