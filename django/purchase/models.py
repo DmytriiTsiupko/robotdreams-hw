62ee01dcce4e1462279885930ce5a63371bd17c7
DmytriiTsiupko <tsiupkodmitrii@gmail.com>
2023-05-03 21:07:24 +0200

    Migrations created.

--- django/purchase/models.py
+++ django/purchase/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from user.models import User
+from book.models import Book
 
-# Create your models here.
+
+class Purchase(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    total_price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
+    order_date = models.DateTimeField(auto_now_add=True)
+
+    # class Meta:
+    #     ordering = ['-order_date']
